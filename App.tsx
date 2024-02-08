1fc96850d641a9976fcfe321c6683a9f4e9e5821
Caio <cararuna@gmail.com>
2024-02-08 00:03:31 -0300

    feat: :sparkles: Ajusta aparencia da Statusbar do mobile e engloba app com rotas
    Signed-off-by: Caio <cararuna@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Routes from "./src/routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>WORKING!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor="#38A69D" barStyle="light-content" />
+      <Routes />
+    </NavigationContainer>
   );
 }
 
@@ -14,6 +15,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  redCard: {
+    flex: 1,
+    backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
   },
